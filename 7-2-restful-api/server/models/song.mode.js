6f707cbba36c30aa478bab08cc5e1624a2563af2
KhawlaAlmalki <s202247320@kfupm.edu.sa>
2025-11-19 12:02:02 +0300

    TODO 1,2

---
+++ 7-2-restful-api/server/models/song.mode.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+    title:  { type: String, required: true, trim: true },
+    artist: { type: String, required: true, trim: true },
+    year:   { type: Number, min: 1900, max: 2100 }
+}, { timestamps: true });
+
+const Song = mongoose.model("Song", songSchema);
+
+export default Song;
